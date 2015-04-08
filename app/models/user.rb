a4736058a3ba1c9798c3d78efd276919ccb4a03c
Aristóteles <aristotelesbr2014@gmail.com>
2015-04-08 08:36:59 -0300

    Capitulo 6 completo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
 	EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+	
 	validates_presence_of :email, :full_name, :location, :password
 	validates_confirmation_of :password
 	validates_length_of :bio, minimum: 30, allow_blank: false
-	validates_uniqueness_of :email
-
-	validate do
-		errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
-	end
+	validates_format_of :email, with: EMAIL_REGEXP
+	
+	has_secure_password
 end
