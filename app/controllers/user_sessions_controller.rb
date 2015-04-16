8fcf79575140dc8d7507fbc0ce277843e4f0d7d4
Aristóteles <aristotelesbr2014@gmail.com>
2015-04-16 12:55:18 -0300

    Terminado capitulo 9.3

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,5 +1,13 @@
 class UserSessionsController < ApplicationController
 	def new
-		@user_session = UserSession.new		
+		@user_session = UserSession.new(session)
+	end
+
+	def create
+		if @user_session.authenticate!
+			redirect_to root_path, notice: t('flash.notice.signed_in')
+		else
+			render :new
+		end
 	end
 end
